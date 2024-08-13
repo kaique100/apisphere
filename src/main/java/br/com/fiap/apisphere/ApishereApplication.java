f3c38f46a7172ef6fb53bae2f767249a679a4f0c
Kaique <skaique5424@gmail.com>
2024-08-13 14:49:28 -0300

    subindo o projeto inicializado na aula remota

---
+++ src/main/java/br/com/fiap/apisphere/ApishereApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.apisphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApishereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApishereApplication.class, args);
+    }
+
+}
